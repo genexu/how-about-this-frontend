a7f1b1354e4a225809d7d15f2c9f293de74295ff
Gene Xu <gene_xu@trendmicro.com>
2021-11-23 13:28:16 +0800

    Clean up basic structure

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           {/* Global site tag (gtag.js) - Google Analytics */}
           <script
             async
